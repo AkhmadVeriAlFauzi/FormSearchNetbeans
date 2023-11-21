0c88c41f743918e9f3dc8a398e4355f97f9a4d45
verryalf <117804886+AkhmadVeriAlFauzi@users.noreply.github.com>
2023-11-21 12:05:58 +0700

    Add files via upload

---
+++ perbaikanUTS/src/perbaikanuts/konetor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package perbaikanuts;
+
+/**
+ *
+ * @author HP14s
+ */
+public class konetor {
+    
+}
